690fefd6a7b45451a99402e009f230d92957988b
Andrew Mason <andrew.3mason@gmail.com>
2017-11-07 19:23:54 -0800

    Updated script.js
    - added tags property
    - added tags property to the document
    - added setInterval method

--- js/script.js
+++ js/script.js
@@ -5,37 +5,43 @@ var quotes = [
     quote: "The only thing to fear is fear itself.",
     source: "Franklin Delano Roosevelt",
     citation: "First Inaugural Address",
-    year: "1932"
+    year: "1932",
+    tags: "politics"
   },
   {
     quote: "That\'s one small step for man, one giant leap for mankind.",
     source: "Neil Armstrong",
     citation: "The moon",
-    year: "1969"
+    year: "1969",
+    tags: "explorer"
   },
   {
     quote: "It always seems impossible until it is done.",
     source: "Nelson Mandela",
     citation: "",
-    year: ""
+    year: "",
+    tags: "politics"
   },
   {
     quote: "Not everything that can be counted counts, and not everything that counts can be counted.",
     source: "Albert Einstein",
     citation: "",
-    year: ""
+    year: "",
+    tags: "science"
   },
   {
     quote: "What we think, we become",
     source: "Buddha",
     citation: "",
-    year: ""
+    year: "",
+    tags: "religion"
   },
   {
     quote: "Dream big and dare to fail",
     source: "Norman Vaughan",
     citation: "",
-    year: ""
+    year: "",
+    tags: "explorer"
   }
 ];
 
@@ -56,12 +62,14 @@ function printQuote() {
   if (randomQuote.year) {
     html += '<span class="year">' + randomQuote.year + '</span>';
   }
+  html += '<span class="tags">, ' + randomQuote.tags + '</span>';
   html += '</p>';
 
   document.getElementById('quote-box').innerHTML = html;
 }
 
-printQuote();
+// Use the setInterval method to change the quote every 10 seconds
+setInterval(printQuote, 10000);
 
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
