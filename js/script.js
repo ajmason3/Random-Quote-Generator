13ef8dd9be21ba15cf035435ae80a6d4294fc7d4
Andrew Mason <andrew.3mason@gmail.com>
2017-11-07 19:13:31 -0800

    Updated script.js
    - added getRandomQuote function
    - added printQuote function

--- js/script.js
+++ js/script.js
@@ -39,6 +39,30 @@ var quotes = [
   }
 ];
 
+// Generating a random number to select a random quote
+function getRandomQuote() {
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  var randQuote = quotes[randomNumber];
+  return randQuote;
+}
+
+function printQuote() {
+  var randomQuote = getRandomQuote();
+  var html = '<p class="quote">' + randomQuote.quote + '</p>';
+  html += '<p class="source">' + randomQuote.source;
+  if (randomQuote.citation) {
+    html += '<span class="citation">' + randomQuote.citation + '</span>';
+  }
+  if (randomQuote.year) {
+    html += '<span class="year">' + randomQuote.year + '</span>';
+  }
+  html += '</p>';
+
+  document.getElementById('quote-box').innerHTML = html;
+}
+
+printQuote();
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
